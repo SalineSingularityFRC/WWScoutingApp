6645355a98d2f29dd6706be78fe03198fdeba3f4
FeatherFawkes <75906634+FeatherFawkes@users.noreply.github.com>
2022-04-12 11:42:04 -0400

    first commit

---
+++ app/src/main/java/team/singularity/wwscoutingapp/MainActivity.java
@@ -0,0 +1,14 @@
+package team.singularity.wwscoutingapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
